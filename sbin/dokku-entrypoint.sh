4c86244db76af35f1b3364a0f8e4c3255367db72
Dio <contact@dionysio.com>
2018-09-25 22:24:52 +0200

    fixing typo

--- sbin/dokku-entrypoint.sh
+++ sbin/dokku-entrypoint.sh
@@ -4,7 +4,7 @@ if [ "${DB_TYPE}" = "postgresql" ]
 then
     export DB_TYPE="postgres"
 fi
-export DB_HOST=="${DB_HOST}:${DB_PORT}"
+export DB_HOST="${DB_HOST}:${DB_PORT}"
 export DB_PASSWD="${DB_PASS}"
 
 /usr/bin/entrypoint "$@"
